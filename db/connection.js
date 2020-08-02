7b87485b10ed33de9d63aa49f53a2443dcf0fcda
JDM-B550 <jendri@jdm-b550.home>
2020-08-01 20:59:13 -0400

    add mongo connection config

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,16 @@
 
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.mongoURI, { useNewUrlParser: true , useUnifiedTopology: true} )
+mongoose.connect(process.env.mongoURI, { useNewUrlParser: true , useUnifiedTopology: true} , () => {
+    console.log("CONNECTED TO MONGO");
+})
+
+//CONNECT TO DATABASE
+  
+//CONNECTION MESSAGING
+db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+db.on("connected", () => console.log("mongo connected!"));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
 mongoose.Promise = Promise
 
--- server.js
+++ server.js
@@ -24,6 +24,9 @@ const corsOptions = {
   },
 };
 
+
+
+
 // MIDDLEWARE
 
 NODE_ENV === "development" ? app.use(cors()) : app.use(cors(corsOptions));
