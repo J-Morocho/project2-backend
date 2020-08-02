8022b1bc776d650261e7d419efc4b50b8035466f
JDM-B550 <jendri@jdm-b550.home>
2020-08-02 18:37:59 -0400

    push to test getUserEventList controller

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -2,6 +2,7 @@ require('../db/connection')
 const {Schema} = require('mongoose')
 const User = require('../models/user')
 const Event = require('../models/event')
+const e = require('express')
 
 const getAllUsers = async (req, res) => {
     try {
@@ -10,11 +11,34 @@ const getAllUsers = async (req, res) => {
     } catch (error) {
         res.status(400).send(error)
     }
+    // FROM PIZZARATS
+    //INDEX - Get all the rats with their pizza
+// const index = async (req, res) => {
+//     try {
+//       //get array rats with pizza ids
+//       const allRats = await Rat.find({});
+//       //create a new array of rats with the pizza info
+//       const rats = allRats.map(async (rat) => {
+//         const thePizza = await Pizza.findById(rat.pizza);
+//         return {
+//           _id: rat._id,
+//           name: rat.name,
+//           pizza: thePizza,
+//         };
+//       });
+//       console.log(rats);
+//       const rats2 = await Promise.all(rats);
+//       console.log(rats2)
+//       await res.status(200).json(rats2);
+//     } catch (error) {
+//       res.status(400).send(error);
+//     }
+//   };
 }
 
 const getUserByName = async(req, res) => {
     try {
-        const user = await User.find({"name":req.params.name})
+        const user = await User.findOne({"name":req.params.name})
         res.status(200).json(user)
     } catch (error) {
         res.status(400).send(error)
@@ -34,11 +58,14 @@ const createUser = async(req, res) => {
 // gets user events
 const getUserEventList = async (req, res) => {
     try {
-        const user = await User.findOne({name: req.params.name}, {eventsAttending:1})
-        res.status(200).json(user)
+        const user = await User.findOne({"name": req.params.name})
+        const arr = await user.eventsAttending
+
+        res.status(200).send(arr)
     } catch (error) {
        res.status(400).send(error) 
     }
+
 }
 
 // Event objectId "5f2571244dc9b91dddd21d06"
@@ -49,7 +76,7 @@ const addEventToUserList = async(req, res) => {
         // findOne is a perfect fit for this function
         const user = await User.updateOne(
             {name:req.params.name},
-            {$addToSet: {eventsAttending: req.params.eventObjectId}})
+            {$addToSet: {eventsAttending: ObjectId(req.params.eventObjectId)}}).populate(Event)
 
         res.status(200).json(user)
     } catch (error) {
--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const {Schema, model} = require('mongoose')
 // In eventsAttending the objectId must already be present in the database
 const userSchema = new Schema({
     name: String,
-    eventsAttending: [{type: Schema.Types.ObjectId, ref: 'event'}]
+    eventsAttending: [{type: Schema.Types.ObjectId, ref: 'events'}]
 })
 
 module.exports = model('user', userSchema)
